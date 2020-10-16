d95f27b9a05b34cd7584f53f49d9b66afc540488
程方 <eos_3tion@163.com>
2020-10-16 17:28:26 +0800

    迁移在浏览器环境下复制数据到剪贴板的方法

---
+++ src/core/utils/DomCopy.ts
@@ -0,0 +1,23 @@
+const copyDiv = document.createElement("div");
+const doc = document;
+const de = doc.documentElement;
+/**
+ * 浏览器环境下，拷贝到剪贴板中
+ * 
+ * @author gushuai
+ * @export
+ * @param {string} str 
+ * @returns 
+ */
+export function doCopy(str: string) {
+    de.appendChild(copyDiv);
+    copyDiv.innerText = str;
+    let selection = getSelection();
+    let range = doc.createRange();
+    range.selectNodeContents(copyDiv);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    let val = doc.execCommand("copy");
+    de.removeChild(copyDiv);
+    return val;
+}
