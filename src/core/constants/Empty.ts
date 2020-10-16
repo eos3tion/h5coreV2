bf092a8d8cba6e7698c1643790b1f575201da72a
程方 <eos_3tion@163.com>
2020-10-16 15:40:33 +0800

    附加几种空方法

--- src/core/constants/Empty.ts
+++ src/core/constants/Empty.ts
@@ -2,4 +2,12 @@ export const Empty = Object.freeze({});
 
 export const EmptyArray = Object.freeze([]);
 
-export const Noop = function () { }
+export const Noop = function () { };
+
+export const Pipe = function <T>(arg: T): T {
+    return arg;
+}
+
+export const TruthyFun = function () { return true }
+
+export const FalsyFun = function () { return false }
