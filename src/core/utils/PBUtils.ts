4d45d635305b3bbba0ffbdb1010e833f327a5ce4
程方 <eos_3tion@163.com>
2020-10-20 15:02:43 +0800

    将定义抽到定义文件中，方便合并扩展

--- src/core/utils/PBUtils.ts
+++ src/core/utils/PBUtils.ts
@@ -5,97 +5,6 @@ const enum StringConst {
     defKey = "def"
 }
 
-export const enum PBType {
-    Double = 1,
-    Float,
-    Int64,
-    UInt64,
-    Int32,
-    Fixed64,
-    Fixed32,
-    Bool,
-    String,
-    Group,
-    Message,
-    Bytes,
-    Uint32,
-    Enum,
-    SFixed32,
-    SFixed64,
-    SInt32,
-    SInt64
-}
-
-/**
- * protobuf2 的字段类型
- * 
- * @export
- * @enum {number}
- */
-export const enum PBFieldType {
-    Optional = 1,
-    Required,
-    Repeated
-}
-
-/**
- * 单个Field的结构
- * 
- * @interface PBField
- */
-export interface PBField extends Array<any> {
-    /**
-     * 
-     * 必有 属性名字
-     * @type {Key}
-     */
-    0: Key;
-    /**
-     * 
-     * 必有 required optional repeated
-     * @type {PBFieldType}
-     */
-    1: PBFieldType;
-    /**
-     * 
-     * 必有 数据类型
-     * @type {number}
-     */
-    2: number;
-    /**
-     * 
-     * 可选 消息类型名称
-     * @type {(Key | PBStruct)}
-     * @memberOf PBField
-     */
-    3?: Key | PBStruct;
-    /**
-     * 可选 默认值
-     * 
-     * @type {*}
-     */
-    4?: any;
-}
-
-/**
- * 单条消息的定义
- * 
- * @interface PBStruct
- */
-export interface PBStruct {
-    /**索引 */
-    [index: number]: PBField;
-    /**
-     * 有默认值的key
-     * 
-     * @type {any}
-     * @memberOf PBStruct
-     */
-    def?: any;
-
-    ref?: { new(): any, prototype: any };
-}
-
 /**
  * PB结构字典
  * 
@@ -103,17 +12,6 @@ export interface PBStruct {
  */
 declare type PBStructDict = {
     [index: string]: PBStruct;
-}
-
-export interface PBStructDictInput {
-    /**
-     * 是否初始化过
-     * 
-     * @type {*}
-     * @memberOf PBStructDict
-     */
-    $$inted?: any;
-    [index: string]: PBStruct | Key;
 }
 
 
