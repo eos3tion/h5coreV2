824a3aae3ec5bc7abd0bf4571e05f1b258b83bde
程方 <eos_3tion@163.com>
2020-10-20 14:47:53 +0800

    增加Service基类

---
+++ src/core/mvc/core/Service.ts
@@ -0,0 +1,105 @@
+import { NetData } from "../../net/NetData";
+import { INetHandler } from "../../net/NetRouter";
+import { NetService } from "../../net/NetService";
+import { Proxy } from "./Proxy";
+
+/**
+ * 用于和服务端通信的数据
+ * @author 3tion
+ */
+export abstract class Service extends Proxy {
+
+    _ns: NetService;
+
+    constructor(name: string | number) {
+        super(name);
+    }
+
+    public onRegister() {
+        this._ns = NetService.get();
+        this.doReg();
+    }
+
+    protected doReg() {
+        //子类重写
+    }
+
+    _startSync(): void {
+        // Service默认为同步，如果需要收到服务端数据的，重写此方法
+        this.selfReady();
+    }
+
+    /**
+     * 用于新版本的自动生成代码的注册
+     * [cmd,msgType,handler,cmd1,msgType1,handler1,cmd2,msgType2,handler2,....cmdN,msgTypeN,handlerN]
+     * @protected
+     * @param {any} args 
+     */
+    protected reg(...args: any[]): void
+    protected reg() {
+        let ns = this._ns;
+        let args = arguments;
+        for (let i = 0; i < args.length; i += 3) {
+            let cmd = args[i];
+            let ref = args[i + 1];
+            let handler = args[i + 2] as INetHandler;
+            handler = handler.bind(this);
+            if (Array.isArray(cmd)) {
+                for (let i = 0; i < cmd.length; i++) {
+                    doReg(ns, cmd[i], handler, ref);
+                }
+            } else {
+                doReg(ns, cmd, handler, ref);
+            }
+        }
+        function doReg(ns: NetService, cmd: number, handler: INetHandler, ref: Key) {
+            ns.register(cmd, handler);
+            ns.regReceiveMSGRef(cmd, ref);
+        }
+    }
+
+    /**
+     * 注册消息引用
+     * 
+     * @protected
+     * @param {string | number} ref 消息实例的引用
+     * @param cmds 注册的指令
+     */
+    protected regMsg(ref: string | number, ...cmds: any[]) {
+        let ns = this._ns;
+        for (let cmd of cmds) {
+            ns.regReceiveMSGRef(cmd, ref);
+        }
+    }
+
+    /**
+     * 注册消息处理函数
+     * 
+     * @protected
+     * @param {{ (data: NetData): void }} handler   消息处理函数
+     * @param {number[]} cmds 注册的指令
+     */
+    protected regHandler(handler: { (data: NetData): void }, ...cmds: number[]) {
+        let ns = this._ns;
+        for (let cmd of cmds) {
+            ns.register(cmd, handler);
+        }
+    }
+
+    protected removeHandler(cmd: number, handler: INetHandler) {
+        this._ns.remove(cmd, handler)
+    }
+
+    /**
+     * 发送消息
+     * 
+     * @protected
+     * @param {number} cmd 指令
+     * @param {any} [data] 数据，简单数据(number,boolean,string)复合数据
+     * @param {string} [msgType] 如果是复合数据，必须有此值
+     * @param {number} [limit=200] 最短发送时间
+     */
+    protected send(cmd: number, data?: any, msgType?: string | number, limit = 200) {
+        this._ns.send(cmd, data, msgType, limit);
+    }
+}
