f7ee72f9c5f9cf7027049b012fe680b477dee102
程方 <eos_3tion@163.com>
2020-10-20 20:03:52 +0800

    扩展Egret部分逻辑

---
+++ src/egret/sui/EgretExtend.ts
@@ -0,0 +1,49 @@
+import { EmptyArray } from "../../core/constants/Empty";
+const { Bitmap, TextField, Graphics } = egret;
+
+let bpt = Bitmap.prototype;
+bpt.refreshBMD = function () {
+    let tex = this.texture;
+    if (tex != null) {
+        this.texture = null;
+        this.texture = tex;
+    }
+}
+/**重写Bitmap.prototype.$refreshImageData用于支持egret的webgl渲染 */
+let $rawRefreshImageData = bpt.$refreshImageData;
+bpt.$refreshImageData = function (this: egret.Bitmap) {
+    $rawRefreshImageData.call(this);
+    let bmd = this.$bitmapData;
+    if (bmd) {
+        this.$sourceWidth = bmd.width;
+        this.$sourceHeight = bmd.height;
+        this.$updateRenderNode();
+    }
+}
+const htmlTextParser = new egret.HtmlTextParser();
+TextField.prototype.setHtmlText = function (this: egret.TextField, value?: string | number) {
+    if (value == undefined) {
+        value = "";
+    } else if (typeof value == "number") {
+        value = value + "";
+    }
+    this.textFlow = value ? htmlTextParser.parser(value) : EmptyArray as egret.ITextElement[];
+}
+
+
+Graphics.prototype.drawRectangle = function (this: egret.Graphics, rect: Rect) {
+    this.drawRect(rect.x, rect.y, rect.width, rect.height);
+}
+
+/**
+ * 移除可视对象
+ * 
+ * @export
+ * @param {egret.DisplayObject} display
+ */
+export function removeDisplay(display: egret.DisplayObject, fire = true) {
+    if (display && display.parent) {
+        display.parent.removeChild(display, fire);
+    }
+}
+
---
+++ src/egret/sui/EgretExtendDefine.d.ts
@@ -0,0 +1,81 @@
+
+
+declare module egret {
+    export interface Bitmap {
+        /**
+         * 刷新纹理
+         */
+        refreshBMD(): void;
+
+        /**
+         * 占位用纹理
+         * 
+         */
+        placehoder?: egret.Texture;
+    }
+
+    export interface DisplayObject {
+        /**
+         * 扩展sui的可视对象，的原始尺寸和坐标  
+         * 由flash导出的原始视图尺寸
+         * @type {SuiRawRect}
+         * @memberOf DisplayObject
+         */
+        suiRawRect?: SuiRawRect;
+        /**
+         * sui的资源名称
+         */
+        suiLib?: string;
+        /**
+         * sui的引用名称
+         */
+        suiClass?: string;
+
+        [name: string]: any;
+    }
+
+    export interface TextField {
+        /**
+         * 原始的文本数据
+         */
+        rawTextData: import("./creator/TextFieldCreator").TextData;
+        /**
+         * 
+         * 设置Html文本(慎用，废性能)
+         * @param {string | number} value
+         */
+        setHtmlText(value: string | number): void;
+    }
+
+    export interface Graphics {
+        /**
+         * 使用  junyou.Rect 作为参数 进行绘制矩形
+         * 
+         * @param { jy.Rect} rect 
+         * @memberof Graphics
+         */
+        drawRectangle(rect: Rect): void;
+    }
+
+    export interface TouchEvent {
+        /** 
+         * 和上一帧的 X偏移量
+         */
+        deltaX?: number;
+        /**
+         * 和上一帧的 Y偏移量
+         */
+        deltaY?: number;
+
+        /**
+         * 和上一帧的 时间差值
+         */
+        deltaTime?: number;
+    }
+
+    export interface DisplayObject extends ViewDele {
+
+    }
+}
+interface SuiRawRect extends egret.Rectangle {
+}
