350b68900d13b4396d9ea6da13fb9e7b6af08916
程方 <eos_3tion@163.com>
2020-10-20 15:01:22 +0800

    增加注释

--- src/core/mvc/core/ViewDele.ts
+++ src/core/mvc/core/ViewDele.ts
@@ -1,20 +1,38 @@
 import { DataEvent } from "../../utils/EventEmitter";
 
-export const enum ViewDeleEvent {
-    OnAwake = "OnAwake",
-    OnSleep = "OnSleep",
-    TouchTap = "TouchTap",
+/**
+ * ViewDele用的事件
+ */
+export const ViewDeleEvent = {
+    OnAwake: "OnAwake",
+    OnSleep: "OnSleep",
+    TouchTap: "TouchTap",
 }
 
 export interface ViewDele {
 
+    /**
+     * 添加事件
+     * @param event 
+     * @param fn 
+     * @param context 
+     */
     on$(event: Key, fn: { (e: DataEvent): any }, context?: any): any;
 
+    /**
+     * 移除事件
+     * @param event 
+     * @param fn 
+     * @param context 
+     */
     off$(event: Key, fn: { (e: DataEvent): any }, context?: any): any;
 
+    /**
+     * 是否可见
+     */
     visible: boolean;
     /**
-     * 是否在显示列表
+     * 是否在显示列表中
      */
     inStage(): boolean;
 }
