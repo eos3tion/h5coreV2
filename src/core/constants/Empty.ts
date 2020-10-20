8d77997fd7701fea88059029423ba74fd5162306
程方 <eos_3tion@163.com>
2020-10-20 15:19:57 +0800

    调整接口

--- src/core/constants/Empty.ts
+++ src/core/constants/Empty.ts
@@ -14,12 +14,13 @@ export function TruthyFun() { return true }
 
 export function FalsyFun() { return false }
 
-export function MustReplace(errorMsg: string) {
-    return function () {
+export function MustReplace<T>(errorMsg: string) {
+    return function (...args: any[]): T {
         if (DEBUG) {
             throw Error(errorMsg);
         } else {
             ThrowError(errorMsg)
         }
+        return
     }
 } 
