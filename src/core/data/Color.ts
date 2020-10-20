d9735457931f3ed05a556990fcb3f2120280c95a
程方 <eos_3tion@163.com>
2020-10-20 19:45:57 +0800

    增加字符串颜色和数值颜色互转的方法

--- src/core/data/Color.ts
+++ src/core/data/Color.ts
@@ -1,3 +1,6 @@
+import { ThrowError } from "../debug/ThrowError";
+import { zeroize } from "../utils/StringUtils";
+
 export class Color {
     r: number;
     g: number;
@@ -42,5 +45,29 @@ export class Color {
 
     color() {
         return new Color(this.r, this.g, this.b, this.a);
+    }
+}
+
+/**
+ * 获取颜色字符串 #a1b2c3
+ * @param c
+ * @return 获取颜色字符串 #a1b2c3
+ *
+ */
+export function getColorString(c: number) {
+    return "#" + zeroize(c.toString(16), 6);
+}
+
+/**
+ * 将#a1b2c3这样#开头的颜色字符串，转换成颜色数值
+ */
+export function getColorValue(c: string) {
+    if (/#[0-9a-f]{6}/i.test(c)) {
+        return +("0x" + c.substring(1));
+    } else {
+        if (DEBUG) {
+            ThrowError(`使用的颜色${c}有误`);
+        }
+        return 0;
     }
 }
