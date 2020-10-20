0e0b349436289ddc1c8a2c7484236a13d45911b7
程方 <eos_3tion@163.com>
2020-10-20 15:02:13 +0800

    增加矩形检查的方法

--- src/core/utils/GeomUtils.ts
+++ src/core/utils/GeomUtils.ts
@@ -8,3 +8,16 @@ export function isLeft(p1: Point, p2: Point, p: Point) {
     let { x, y } = p;
     return (p1.x - x) * (p2.y - y) < (p2.x - x) * (p1.y - y);
 }
+
+/**
+ * 检查`main`Rect是否包含`tester`
+ * @param main 
+ * @param tester 
+ */
+export function containsRect(main: Rect, tester: Rect) {
+    let r1 = tester.x + tester.width;
+    let b1 = tester.y + tester.height;
+    let r2 = main.x + main.width;
+    let b2 = main.y + main.height;
+    return (tester.x >= main.x) && (tester.x < r2) && (tester.y >= main.y) && (tester.y < b2) && (r1 > main.x) && (r1 <= r2) && (b1 > main.y) && (b1 <= b2);
+}
