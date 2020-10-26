ed50b7cfff935d59c67b48d60dc5045350832466
程方 <eos_3tion@163.com>
2020-10-26 20:15:06 +0800

    调整为esmodule代码

--- src/egret/game/unit/MUnitAction.ts
+++ src/egret/game/unit/MUnitAction.ts
@@ -1,27 +1,29 @@
-namespace jy {
+import { IUnitActionInfo } from "./IUnitActionInfo";
+import { MountType } from "./MountType";
+import { UnitAction } from "./UnitAction";
+
+/**
+ * 带坐骑动作的UnitAction基类
+ * @author 3tion
+ */
+export class MUnitAction extends UnitAction {
+
     /**
-     * 带坐骑动作的UnitAction基类
-     * @author 3tion
+     * 动作序列<br/>
+     * 如果没有对应动作，Unit，强制使用有动作的坐骑类型，并切换动作
+     * Key      MountType<br/>
+     * Value    动作的唯一标识<br/>
      */
-    export class MUnitAction extends UnitAction {
+    protected actions: { [index: number]: IUnitActionInfo };
 
-    	/**
-    	 * 动作序列<br/>
-    	 * 如果没有对应动作，Unit，强制使用有动作的坐骑类型，并切换动作
-    	 * Key      MountType<br/>
-    	 * Value    动作的唯一标识<br/>
-    	 */
-        protected actions: { [index: number]: IUnitActionInfo };
+    constructor() {
+        super();
+    }
 
-        constructor() {
-            super();
+    getAction(mountType: MountType): IUnitActionInfo {
+        if (mountType in this.actions) {
+            return this.actions[mountType];
         }
-
-        getAction(mountType: MountType): IUnitActionInfo {
-            if (mountType in this.actions) {
-                return this.actions[mountType];
-            }
-            return UnitAction.defaultAction;
-        }
+        return UnitAction.defaultAction;
     }
 }
